5f55385bdffa4b8db3f24d2fe97726bc547034b2
vv960216 <liyurui960216@gmail.com>
2024-02-02 11:24:46 +0800

    Initial commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+vim.opt['cmdheight'] = 2
+vim.opt['clipboard'] = "unnamedplus"
+vim.opt['mouse'] = 'a'
+vim.opt['undofile'] = true
+vim.opt['tabstop'] = 4
+vim.opt['shiftwidth'] = 4
+vim.opt['cursorline'] = true
+
